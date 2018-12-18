833f03281a6b69622b7baf6b95080975fd1f866c
Zoltan <zolihungary2@gmail.com>
2018-12-18 08:45:48 +0100

    Nothing of use

---
+++ zoltan/FileReverser.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+let file = fs.readFileSync("/Users/zoltan/Desktop/Storage/Othello.rtf","UTF8");
+console.log(file);
+//Output is an array
+//Separate file into lines
+//Put lines into array
+//Reverse the array
+//console.log(output);
+
+
