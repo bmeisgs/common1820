4e267b15a2744f44eb435d06fab2bb54a39b64b5
Zoltan <zolihungary2@gmail.com>
2018-12-19 08:09:19 +0100

    works mostly

--- zoltan/FileReverser.js
+++ zoltan/FileReverser.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 let file = fs.readFileSync("/Users/zoltan/Desktop/Storage/Othello.rtf","UTF8");
 console.log(file);
-//Output is an array
-//Separate file into lines
-//Put lines into array
-//Reverse the array
-//console.log(output);
+reg = /\r?\n/
+let lines = file.split(reg);
+console.log(lines);
 
+//output.reverse(); would be the simple way but you asked for the alternate way to reverse it
 
+let output = [];
+for(let i = 0; i < lines.length; i++) {
+    output.unshift(lines[i]);
+}
+console.log(output);
