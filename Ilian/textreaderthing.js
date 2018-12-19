f825c1d2b82fd0812eb826c9757497dd6e96441d
Ilian <ilianispps@gmail.com>
2018-12-19 08:08:09 +0100

    stuff

--- Ilian/textreaderthing.js
+++ Ilian/textreaderthing.js
@@ -1,9 +1,9 @@
 let arr = []
 const fs = require("fs")
 let readOut = fs.readFileSync("C:/users/Embassy of Albania/Desktop/ITstuff.txt", 'UTF8')
-//pretend i made all the lines in the text seperate strings and pushed them into the array
-arr.push(readOut) // pretend these are seperate indexes
+console.log(readOut)
+let lines = readOut.split("\r\n")
+for(let i=0; i<lines.length; i++){
+    arr.unshift(lines[i])
+}
 console.log(arr)
-let reversearr = arr.reverse
-console.log(reversearr)
-//now pretend i remade those seperate strings into one string and logged it as a string
