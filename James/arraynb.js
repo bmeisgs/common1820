96f0bba598ab4915981db3d357e2c170ec11e267
James <cartwrightjames8862@gmail.com>
2018-12-19 09:01:14 +0100

    Oooh LaLa

---
+++ James/arraynb.js
@@ -0,0 +1,13 @@
+let array = [5, 1, 4, 7, 9, -12]
+
+let length = array.length;
+
+let values = []
+
+for(0; length-1 < array.length; length++){
+   values.push = array[length]-array[length+1]
+}
+
+let display = values[Number.MIN_VALUE]
+
+console.log(display)
