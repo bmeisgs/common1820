972c28de217a132cedbf0af6a021346fb0bc3f24
Andras Kemeny <subpardaemon@gmail.com>
2018-12-20 08:08:27 +0100

    half-baked

---
+++ Teach/scores.js
@@ -0,0 +1,13 @@
+const students = [
+    {name:"May",grade:99},
+    {name:"Eri",grade:55},
+    {name:"Elen",grade:77},
+    {name:"Rit",grade:45},
+    {name:"Rato",grade:89},
+    {name:"More",grade:98},
+    {name:"Epi",grade:76},
+    {name:"Ent",grade:45},
+    {name:"Ronal",grade:33},
+    {name:"Bib",grade:75}
+];
+
