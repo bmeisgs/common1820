845013ed5313ab459abab688de6746f52d5cb17b
James <cartwrightjames8862@gmail.com>
2019-01-10 09:06:21 +0100

    thinking emoji

---
+++ James/weather.js
@@ -0,0 +1,25 @@
+class cityTemp {
+    constructor(name) {
+        this.name = name;
+        this.tempHigh = {};
+        this.tempLow = {};
+    }
+}
+
+const Budapest = new cityTemp('Budapest');
+Budapest.tempHigh = 15.0;
+Budapest.tempLow = 6.0;
+
+const Azgard = new cityTemp('Azgardt');
+Azgard.tempHigh = 25.0;
+Azgard.tempLow = 6.4;
+
+const Bucharest = new cityTemp('Bucharest');
+Bucharest.tempHigh = 17.1;
+Bucharest.tempLow = 5.2;
+
+function cityTemp (tempHigh) {
+    tempHigh.length 
+    for (i=1; i < 10; i++)
+
+}
