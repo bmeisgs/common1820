3baac6bb60f8591132de766ea31b9a97b6317fd9
Ilian <ilianispps@gmail.com>
2019-01-10 09:03:55 +0100

    insert message here

---
+++ Ilian/citytemp.js
@@ -0,0 +1,21 @@
+class City {
+    constructor(cityname){
+        this.totalH = 0
+        this.totalL = 0
+        this.avgH = 0
+        this.avgL = 0
+        
+    }
+}
+
+const Citiesinfo = [
+    {name:"Tirana",Higesttemp:41,Lowesttemp:-10}
+    {name:"Budapest",Higesttemp:56,Lowesttemp:-13}
+    {name:"Moscow",Higesttemp:23,Lowesttemp:-37}
+]
+
+function avgH(Higesttemp, Lowesttemp){
+    let theaveragehigh = (Higesttemp + Lowesttemp) /2
+}
+
+function avgL()
--- README.md
+++
@@ -1,2 +0,0 @@
-# common1820
-Common repo for the class of 18/20
