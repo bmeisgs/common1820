60ab8895aa34ef79c1e02d550bc72007b52e287e
Ilian <ilianispps@gmail.com>
2019-01-16 20:27:12 +0100

    has a problem but idk where

---
+++ Ilian/Sorting4.js
@@ -0,0 +1,20 @@
+let elements = [1,633,8,2,4,1,22,66,20,122];
+let n = 10;
+let i = 0;
+while(i<=n-i-1){
+    let j = 0;
+    while(j <= n-i-2){
+        if(elements[j]<elements[j+1]){
+            let temp = elements[j];
+            elements[j] = elements[j+1];
+            elements[j+1] = temp;
+        }
+        j = j+1;
+    }
+    i = i+1;
+}
+
+console.log("Sorted elements")
+for(let e = 0; e<elements.length-1; e++){
+    console.log(elements[e]);
+}
